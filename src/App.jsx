245e925c57df9fa058f462b11d1d204766d77531
Rama Shankar Jha <guy55222@gmail.com>
2023-09-06 11:49:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='h-screen bg-black text-white flex flex-col justify-center items-center text-3xl'>Vite is running with tailwind</h1>
+    </>
+  )
+}
+
+export default App
